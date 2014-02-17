11ee8033bf72c0b9cdee2816bc72b6af35c427e1
Daniel Shields <d4nshields@gmail.com>
2014-02-16 21:39:02 -0500

    updated jquery and jquery-ui to be same as chrome extension

--- firefox/lib/main.js
+++ firefox/lib/main.js
@@ -6,7 +6,7 @@ var self = require("sdk/self");
           include: "*.youtube.com",
           attachTo: ["existing", "top"],
           contentScriptFile: [
-            self.data.url('jquery.min.js'),
+            self.data.url('jquery.js'),
             self.data.url('jquery-ui.js'),
             self.data.url('myscript.js')
           ],
