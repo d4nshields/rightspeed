6043df070491ee66cd56158ebc1a942a593e234c
Daniel Shields <d4nshields@gmail.com>
2014-03-12 22:27:27 -0400

    changed settings for Google Analytics.  Removed setAllowLinker because it is not needed.   Added setDomainName because it seemed harmless and probably a good idea, if anything.

--- chrome/ga-config.js
+++ chrome/ga-config.js
@@ -1,4 +1,4 @@
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-48191515-2']);
-_gaq.push(['_setAllowLinker', true]);
+_gaq.push(['_setDomainName', 'youtube.com']); 
 _gaq.push(['_trackPageview']);
