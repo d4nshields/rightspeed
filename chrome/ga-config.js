1e05c85b31cdafa0d3d4f4f01455394a13bee8b4
Dan Shields <d4nshields@gmail.com>
2014-04-06 21:55:45 -0400

    re-added _trackPageView so there is not a sudden drop off of page views.

--- chrome/ga-config.js
+++ chrome/ga-config.js
@@ -2,3 +2,4 @@ var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-48191515-2']);
 _gaq.push(['_setDomainName', 'youtube.com']); 
 _gaq.push(['_trackPageview']);
+_gaq.push(['_trackEvent', 'pageView']);
