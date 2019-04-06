8125d380ddf6f0706356831ffcdf475be0b082e2
Daniel Shields <d4nshields@gmail.com>
2019-04-06 15:50:47 -0400

    build script improvements for 0.0.4.* releases

--- chrome/zipup.sh
+++ chrome/zipup.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 rm -f chrome.zip
-zip chrome.zip manifest.json BTTF.ttf stop-watch-icon.png jquery-ui.css spdr-styles.css jquery.min.js jquery-ui.js myscript.js commLink.js stopwatch-top.png button_instant_replay.png images/*
+zip chrome.zip manifest.json background.js background.html popup.html \
+ BTTF.ttf *.png jquery-ui.css spdr-styles.css \
+ stopwatch-top.png button_instant_replay.png images/* \
+ jquery.min.js jquery-ui.js \
+ myscript.js commLink.js ga-config.js
+
